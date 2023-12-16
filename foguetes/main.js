192ada837969516478887367fa5f2731465ca5bb
roniwellington <roniwellington@gmail.com>
2023-12-15 21:53:41 -0300

    Subindo site

---
+++ foguetes/main.js
@@ -0,0 +1,35 @@
+// header scroll
+let header = document.querySelector('header');
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+window.addEventListener('scroll', () => {
+  header.classList.toggle('shadow', window.scrollY > 0);
+})
+
+menu.onclick = () => {
+  menu.classList.toggle('bx-x');
+  navbar.classList.toggle('active')
+}
+window.onscroll = () => {
+  menu.classList.remove('bx-x');
+  navbar.classList.remove('active')
+}
+
+
+var swiper = new Swiper(".home", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    // navigation: {
+    //   nextEl: ".swiper-button-next",
+    //   prevEl: ".swiper-button-prev",
+    // },
+  });
